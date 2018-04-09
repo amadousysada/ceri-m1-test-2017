7c9d51689361422e7c338f3bf77748c781b65c8c
amadousysada <amadousysada@e-mc137-121.univ-avignon.fr>
2018-04-09 11:49:27 +0200

    dernier commit 09/04/2018

---
+++ src/test/java/fr/univavignon/rodeo/IAnimalTest.java
@@ -0,0 +1,40 @@
+package fr.univavignon.rodeo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import fr.univavignon.rodeo.api.IAnimal;
+
+public class IAnimalTest{
+	
+	IAnimal animal;
+	
+	@Before
+	public void getInstance(){
+		animal= mock(IAnimal.class);
+		when(animal.isBoss()).thenReturn(false);
+		when(animal.getName()).thenReturn("Gaby");
+		when(animal.getXP()).thenReturn(12);
+	}
+	
+	@Test
+	public void isBoss(){
+		assertEquals(false,animal.isBoss());
+		assertEquals(true,animal.isBoss());
+	}
+	
+	@Test
+	public void getName(){
+		assertEquals("Gaby", animal.getName());
+		assertEquals("AmadouS", animal.getName());
+	}
+	
+	@Test
+	public void getXP(){
+		assertEquals(12, animal.getXP());
+	}
+
+}
